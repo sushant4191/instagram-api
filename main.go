3d81d33fd8d8c361e562331397d02e318a24cc06
Sushant Srivastav <63559772+sushant4191@users.noreply.github.com>
2021-10-10 00:14:16 +0545

    updated few changes

--- main.go
+++ main.go
@@ -5,32 +5,41 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-
-	"github.com/gin-gonic/gin"
 )
 
 type user struct {
-	ID   string `json:"ID"`
-	Name string `json:"name"`
-	Pass string `json:"pass"`
+	ID    string `json:"ID"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
+	Pass  string `json:"pass"`
 }
 type Users []user
+type post struct {
+	ID        string `json:"ID"`
+	Caption   string `json:"caption"`
+	URL       string `json:"img_url"`
+	Timestamp string `json:"timestamp"`
+}
+type Posts []post
 
 func allUsers(w http.ResponseWriter, r *http.Request) {
 	users := Users{
-		user{ID: "Test ID", Name: "Test Name", Pass: "Test Pass"},
+		user{ID: "Test ID", Name: "Test Name", Email: "Email", Pass: "Test Pass"},
 	}
 
-	fmt.Println("Endpoint Hit: All Articles Endpoint")
+	fmt.Println("Endpoint Hit: All Posts Endpoint")
 	json.NewEncoder(w).Encode(users)
+}
+func allPosts(w http.ResponseWriter, r *http.Request) {
+	posts := Posts{
+		post{ID: "Test ID", Caption: "Test CAP", URL: "testurl", Timestamp: "time"},
+	}
+	fmt.Println("Endpoint Hit: All Posts Endpoint")
+	json.NewEncoder(w).Encode(posts)
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Homepage Endpoint Hit")
-}
-
-func getUsers(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, Users)
 }
 
 func handleRequests() {
@@ -38,7 +47,7 @@ func handleRequests() {
 	//	http.HandleFunc("/articles", allArticles)
 	http.HandleFunc("/users", allUsers)
 	// http.HandleFunc("/users/id", ids)
-	// http.HandleFunc("/posts", posts)
+	http.HandleFunc("/posts", allPosts)
 	// http.HandleFunc("/posts/id", postid)
 
 	log.Fatal(http.ListenAndServe(":8081", nil))
